b07c5440b22474179b8f4ef328f1dabc479783dd
ktsutsu <tsutsu0422@gmail.com>
2013-05-08 14:18:08 +0900

    読み込みボタンクリック時の挙動追加

--- PhotoEffect/MainPage.xaml.cs
+++ PhotoEffect/MainPage.xaml.cs
@@ -9,16 +9,41 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
 
 namespace PhotoEffect
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        PhotoChooserTask task = new PhotoChooserTask();
+
         // コンストラクター
         public MainPage()
         {
             InitializeComponent();
+            task.Completed += new EventHandler<PhotoResult>(task_Completed);
+            task.ShowCamera = true;
+        }
+
+        void task_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult != TaskResult.OK) {
+                return;
+            }
+
+            // 選択した画像をimageコントロールに表示する
+            BitmapImage bmp = new BitmapImage();
+            bmp.SetSource(e.ChosenPhoto);
+            imgPhoto.Source = bmp;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            task.PixelWidth = 440;
+            task.PixelHeight = 440;
+            task.Show();
         }
     }
 }
